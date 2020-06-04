5899921f8bdab46fbbe049d7a9a423175debb447
Waitrum <evstegneych@mail.ru>
2020-06-04 18:25:31 +0700

    #1

--- main.py
+++ main.py
@@ -61,16 +61,18 @@ class Config:
 
 
 class Message:
-    def __init__(self, _user_id, _peer_id, _text, _message_id, _audio):
+    def __init__(self, _user_id, _peer_id, _message_id):
         self.user_id = _user_id
         self.peer_id = _peer_id
         self.name = GetNameUsers(self.user_id) + ":"
-        self.text = _text
+        self.text = ""
+        self.attachments = []
         self.message_id = _message_id
         self.date = datetime.datetime.now()
         self.deleted = False
         self.edited = False
-        self.audio = _audio
+        self.count_edited = 0
+        self.audio = False
 
     def set_deleted(self):
         self.deleted = True
@@ -83,6 +85,9 @@ class Message:
 
     def get_edited(self):
         return "[edited]\n" if self.edited else " "
+
+    def set_audio(self):
+        self.audio = True
 
 
 cfg = Config("config.json")
@@ -102,12 +107,34 @@ def MessagesSend(_peer_id, _text, disable_mentions=1):
     return vk.messages.send(peer_id=_peer_id,
                             message=_text,
                             random_id=random.randint(-1000000, 1000000),
-                            disable_mentions=disable_mentions)
+                            disable_mentions=disable_mentions,
+                            dont_parse_links=1)
 
 
 def MessageDelete(mid, delete_for_all=1):
     vk.messages.delete(message_ids=mid,
                        delete_for_all=delete_for_all)
+
+
+def GetAllAttachments(_res, msg: Message):
+    _arr = []
+    msg.attachments = []
+    for attach in _res:
+        audio_message = attach.get("audio_message")
+        if audio_message:
+            msg.attachments.append(audio_message.get("link_ogg"))
+            msg.set_audio()
+            break
+
+        sticker = attach.get("sticker")
+        if sticker:
+            msg.attachments.append(sticker["images"][len(sticker["images"]) - 1]["url"])
+
+        photo = attach.get("photo")
+        if photo:
+            msg.attachments.append(photo["sizes"][len(photo["sizes"]) - 1]["url"])
+
+    return msg
 
 
 def GetNameUsers(user_ids):
@@ -149,107 +176,101 @@ def clear_db():
 
 db = {}
 
+
 # run(clear_db, timeout=600)
-print("Бот запущен")
-while True:
-    try:
-        for event in longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW:
-                if event.from_chat and event.user_id > 0:
-                    if event.user_id != user_id:
-                        add_text = None
-                        audio_message = None
-                        if event.peer_id not in db:
-                            db[event.peer_id] = []
-                        if event.text:
-                            add_text = event.text[:150]
+def main():
+    print("Бот запущен")
+    while True:
+        try:
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if event.from_chat and event.user_id > 0:
+                        print(event.raw)
+                        if event.user_id != user_id:
+                            msg = Message(event.user_id, event.peer_id, event.message_id)
+                            if event.peer_id not in db:
+                                db[event.peer_id] = []
+                            if event.text:
+                                msg.text = event.text[:150]
+                            else:
+                                if event.peer_id in cfg.WhiteListChat:
+                                    response = vk.messages.getById(message_ids=event.message_id)["items"]
+                                    if response:
+                                        response = response[0]
+                                        attachments = response.get("attachments")
+                                        msg = GetAllAttachments(attachments, msg)
+                            _len = len(db[event.peer_id])
+                            if _len > 200:
+                                db[event.peer_id] = db[event.peer_id][_len - 100:]
+                            db[event.peer_id].append(msg)
                         else:
-                            if event.peer_id in cfg.WhiteListChat:
+                            if not event.text:
+                                continue
+                            message = event.message.lower()
+
+                            if message.startswith(cfg.Trigger):
+                                cmd = message[len(cfg.Trigger):].strip()
+                                show_only_deleted = cmd == "+"
                                 response = vk.messages.getById(message_ids=event.message_id)["items"]
+                                get_user_id = None
                                 if response:
                                     response = response[0]
-                                    attach = response.get("attachments")
-                                    if attach:
-                                        sticker = attach[0].get("sticker")
-                                        if sticker:
-                                            add_text = sticker["images"][len(sticker["images"]) - 1]["url"]
-                                        else:
-                                            add_text = "[Вложение]"
+                                    reply_message = response.get("reply_message")
+                                    fwd_messages = response.get("fwd_messages")
+                                    if reply_message:
+                                        get_user_id = reply_message["from_id"]
+                                    elif fwd_messages:
+                                        get_user_id = fwd_messages[0]["from_id"]
 
-                                        audio_message = attach[0].get("audio_message")
-                                        if audio_message:
-                                            add_text = audio_message.get("link_ogg")
-
-                            else:
-                                add_text = "[Вложение]"
-                        db[event.peer_id] = db[event.peer_id][len(db[event.peer_id]) - 25:]
-                        db[event.peer_id].append(
-                            Message(event.user_id, event.peer_id, add_text, event.message_id, bool(audio_message)))
-                    else:
-                        if not event.text:
-                            continue
-                        message = event.message.lower()
-
-                        if message.startswith(cfg.Trigger):
-                            cmd = message[len(cfg.Trigger):].strip()
-                            show_only_deleted = cmd == "+"
-                            response = vk.messages.getById(message_ids=event.message_id)["items"]
-                            get_user_id = None
-                            if response:
-                                response = response[0]
-                                reply_message = response.get("reply_message")
-                                fwd_messages = response.get("fwd_messages")
-                                if reply_message:
-                                    get_user_id = reply_message["from_id"]
-                                elif fwd_messages:
-                                    get_user_id = fwd_messages[0]["from_id"]
-                                else:
-                                    get_user_id = None
-
-                            text = f"Лог {GetNameUsers(get_user_id) if get_user_id else ''}:\n"
-                            arr = db.get(event.peer_id, [])
-                            logs = []
-                            for user in arr:
-                                if user.user_id == get_user_id or not get_user_id:
-                                    logs.append(user)
-                            for user in logs[len(logs) - 10:]:
-                                if (show_only_deleted and user.deleted) or not show_only_deleted:
+                                text = f"Лог {GetNameUsers(get_user_id) if get_user_id else ''}:\n"
+                                arr = db.get(event.peer_id, [])
+                                logs = []
+                                for user in arr:
+                                    if user.user_id == get_user_id or not get_user_id:
+                                        if (show_only_deleted and user.deleted) or not show_only_deleted:
+                                            logs.append(user)
+                                for user in logs[len(logs) - 10:]:
                                     text += f"{user.name if not get_user_id else '--'} {user.get_edited()}" \
                                             f"{user.get_deleted()} {user.text}" \
                                             f"\n"
-                            MessagesSend(event.peer_id, text)
-                            MessageDelete(event.message_id)
+                                MessagesSend(event.peer_id, text)
+                                MessageDelete(event.message_id)
 
-                        if message == cfg.TriggerToAddChat:
-                            if event.peer_id in cfg.WhiteListChat:
-                                cfg.WhiteListChat.remove(event.peer_id)
-                                MessageEdit(event.message_id, f"Беседа <<{event.peer_id}>> удалена.", event.peer_id)
-                            else:
-                                cfg.WhiteListChat.append(event.peer_id)
-                                MessageEdit(event.message_id, f"Беседа <<{event.peer_id}>> добавлена.",
-                                            event.peer_id)
-                            run(target=MessageDelete, arg=[event.message_id], timeout=5)
-                            cfg.update()
-                            cfg.save()
+                            if message == cfg.TriggerToAddChat:
+                                if event.peer_id in cfg.WhiteListChat:
+                                    cfg.WhiteListChat.remove(event.peer_id)
+                                    MessageEdit(event.message_id, f"Беседа <<{event.peer_id}>> удалена.", event.peer_id)
+                                else:
+                                    cfg.WhiteListChat.append(event.peer_id)
+                                    MessageEdit(event.message_id, f"Беседа <<{event.peer_id}>> добавлена.",
+                                                event.peer_id)
+                                run(target=MessageDelete, arg=[event.message_id], timeout=5)
+                                cfg.update()
+                                cfg.save()
 
-            if event.type == VkEventType.MESSAGE_FLAGS_SET and event.raw[0] == 2:
-                if event.peer_id in db:
-                    for user in db.get(event.peer_id, []):
-                        if user.message_id == event.message_id:
-                            user.set_deleted()
+                if event.type == VkEventType.MESSAGE_FLAGS_SET and event.raw[0] == 2:
+                    if event.peer_id in db:
+                        for user in db.get(event.peer_id, []):
+                            if user.message_id == event.message_id:
+                                user.set_deleted()
 
-            if event.type == VkEventType.MESSAGE_EDIT and event.raw[0] == 5:
-                if event.peer_id in db:
-                    for user in db.get(event.peer_id, []):
-                        if user.message_id == event.message_id and not user.audio:
-                            user.text += f"\n↓\n{event.text}"
-                            user.edited = True
+                if event.type == VkEventType.MESSAGE_EDIT and event.raw[0] == 5:
+                    if event.peer_id in db:
+                        for user in db.get(event.peer_id, []):
+                            if user.message_id == event.message_id and not user.audio and user.count_edited <= 5:
+                                user.text += f"\n↓\n{event.text[:100]}"
+                                user.edited = True
+                                user.count_edited += 1
+                            if user.count_edited == 5:
+                                user.text += f"\n[Редактирований > 5]"
 
-    except ReadTimeout:
-        pass
+        except ReadTimeout:
+            pass
 
-    except Exception as e:
-        print("Основной поток: ", e)
-        time.sleep(10)
+        except Exception as e:
+            print("Основной поток: ", e)
+            time.sleep(10)
 
-# Messages.select().where(Messages.peer_id == event.peer_id).order_by(Messages.id.desc()).limit(10)
+
+if __name__ == '__main__':
+    main()
